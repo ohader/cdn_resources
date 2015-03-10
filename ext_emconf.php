a157462ed6399360b837f06c82a53f6c8ce4557f
Oliver Hader <oliver@typo3.org>
2015-03-10 17:23:11 +0100

    [TASK] Raise version number to 0.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,9 +29,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
